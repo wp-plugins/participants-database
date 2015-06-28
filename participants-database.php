dae128a30232cd351b52218158dce969f0117339
xnau <xnau@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-28 02:43:58 +0000

    Updates to Update_Notices class
    ready for release
    
    git-svn-id: https://plugins.svn.wordpress.org/participants-database/trunk@1188818 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- participants-database.php
+++ participants-database.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://xnau.com/wordpress-plugins/participants-database
  * Description: Plugin for managing a database of participants, members or volunteers
  * Author: Roland Barker
- * Version: 1.6
+ * Version: 1.6.0
  * Author URI: http://xnau.com
  * License: GPL2
  * Text Domain: participants-database
